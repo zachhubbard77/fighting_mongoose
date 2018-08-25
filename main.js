25078cd2851fe901604847424ca6122a7333bfa3
havonovich <haven.kardel@gmail.com>
2018-08-25 10:41:04 -0700

    intitial js

---
+++ main.js
@@ -0,0 +1,80 @@
+let num0 = document.getElementById("num0");// begin variables for event listeners/handlers
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let num3 = document.getElementById("num3");
+let num4 = document.getElementById("num4");
+let num5 = document.getElementById("num5");
+let num6 = document.getElementById("num6");
+let num7 = document.getElementById("num7");
+let num8 = document.getElementById("num8");
+let num9 = document.getElementById("num9");
+let add = document.getElementById("add");
+let subtract = document.getElementById("subtract");
+let multiply = document.getElementById("multiply");
+let divide = document.getElementById("divide");
+let clear = document.getElementById("clear");
+let equals = document.getElementById("equals");//end variables for event listeners
+num0.addEventListener("click",function(){//begin event listeners for keys
+
+    console.log("num0");
+})
+num1.addEventListener("click",function(){
+
+    console.log("num1");
+})
+num2.addEventListener("click",function(){
+
+    console.log("num2");
+})
+num3.addEventListener("click",function(){
+
+    console.log("num3");
+})
+num4.addEventListener("click",function(){
+
+    console.log("num4");
+})
+num5.addEventListener("click",function(){
+
+    console.log("num5");
+})
+num6.addEventListener("click",function(){
+
+    console.log("num6");
+})
+num7.addEventListener("click",function(){
+
+    console.log("num7")
+})
+num8.addEventListener("click",function(){
+
+    console.log("num8");
+})
+num9.addEventListener("click",function(){
+
+    console.log("num9");
+})
+add.addEventListener("click",function(){
+
+    console.log("add");
+})
+subtract.addEventListener("click",function(){
+
+    console.log("subtract");
+})
+multiply.addEventListener("click",function(){
+
+    console.log("multiply");
+})
+divide.addEventListener("click",function(){
+
+    console.log("divide");
+})
+clear.addEventListener("click",function(){
+
+    console.log("clear");
+})
+equals.addEventListener("click",function(){
+
+    console.log("equals");
+})//end event listeners for keys
